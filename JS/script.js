3599327411ddcdf3c237714fac7d3f62a1698434
Matthew Crowell <overthewelldesigns@gmail.com>
2020-03-04 11:57:02 -0700

    added enemy types, level switch,
    allowed user to switch through 3 different levels (still to be designed) and added the peaceable sprites for each enemy (still need kill logic) 
    
    still need to add moving enemy types

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-/*
+//timer setup
 var m = document.getElementById("min");//the variable for the minutes
 var s = document.getElementById("sec");//the variable for the seconds
 var total = 0;//makes the timer start at 0
@@ -18,90 +18,202 @@ return "0" + valuestring;
 return valuestring;
 }
 }
-*/
-var canvas = $('#GameBoardCanvas');
-//The game board 1 = walls, 0 = free space, and -1 = the goal
-var boardOne = [
-    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
-    [ 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1],
-    [ 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1],
-    [ 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
-    [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
-    [ 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
-    [ 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1],
-    [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1]
-];
-var player = {
-    x: 0,
-    y: 0
-};
 
+
+
+//game start
+var loadOne = true;
+var loadTwo = false;
+var loadThree = false;
+//The game board 1 = walls, 0 = free space, and -1 = the goal, 2 = chest
+
+    var boardOne = [
+        [ 0, 5, 4, 3, 2, 0, 0, 0, 0, 0, 1],
+        [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+        [ 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1],
+        [ 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+        [ 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 2, 1, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1]
+    ];
+
+    var boardTwo = [
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+        [ 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1],
+        [ 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1],
+        [ 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 2, 1, 1, 0, 1],
+        [ 1, 1, 1, 0, 1, -1, 0, 0, 0, 0, 1]
+    ];
+
+
+    var boardThree = [
+        [ 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+        [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+        [ 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1],
+        [ 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+        [ 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 2, 1, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, -1, 0, 0, 0, 0, 1]
+    ];
+    var player = {
+        x: 0,
+        y: 0
+    };
+
+    board = boardOne
 //Draw the game board
 function draw(){
+    var canvas = $('#GameBoardCanvas');
     var width = canvas.width();
+    var height = canvas.height();
     var blockSizex = width/11;
-    var blockSizey = width/10;
+    var blockSizey = height/board.length;
     var ctx = canvas[0].getContext('2d');
-    ctx.setTransform(1, 0, 0, 1, 0, 0, 0);
-    ctx.clearRect(0, 0, width, width);
+    ctx.clearRect(0, 0, width, height);
     ctx.fillStyle="white";
     //Loop through the board array drawing the walls and the goal
-    for(var y = 0; y < boardOne.length; y++){
-        for(var x = 0; x < boardOne[y].length; x++){
+    for(var y = 0; y < board.length; y++){
+        for(var x = 0; x < board[y].length; x++){
             //Draw a wall
-            if(boardOne[y][x] === 1){
+            if(board[y][x] === 1)
+            {
                 ctx.fillRect(x*blockSizex, y*blockSizey, blockSizex, blockSizey);
             }
             //Draw the goal
-            else if(boardOne[y][x] === -1){
-                ctx.beginPath();
-                ctx.lineWidth = 5;
-                ctx.strokeStyle = "gold";
-                ctx.moveTo(x*blockSizex, y*blockSizey);
-                ctx.lineTo((x+1)*blockSizex, (y+1)*blockSizey);
-                ctx.moveTo(x*blockSizex, (y+1)*blockSizey);
-                ctx.lineTo((x+1)*blockSizex, y*blockSizey);
-                ctx.stroke();
+            else if(board[y][x] === -1)
+            {
+                var img = document.getElementById("win");
+                ctx.drawImage(img,x*blockSizex, y*blockSizey, blockSizex, blockSizey);
+
             }
+            //Draw the extra coins
+            else if(board[y][x] === 2)
+            {
+                var img = document.getElementById("point");
+                ctx.drawImage(img,x*blockSizex, y*blockSizey, blockSizex, blockSizey);
+            }
+
+            //Draw the first enemy
+            else if(board[y][x] === 3)
+            {
+                var img = document.getElementById("emenyFour");
+                ctx.drawImage(img,x*blockSizex, y*blockSizey, blockSizex, blockSizey);
+            }
+            //Draw the second enemy
+            else if(board[y][x] === 4)
+            {
+                var img = document.getElementById("enemyTwo");
+                ctx.drawImage(img,x*blockSizex, y*blockSizey, blockSizex, blockSizey);
+            }
+             //Draw the third enemy
+             else if(board[y][x] === 5)
+             {
+                var img = document.getElementById("enemyThree");
+                ctx.drawImage(img,x*blockSizex, y*blockSizey, blockSizex, blockSizey);
+            }
+            
+
+            
         }
     }
     //Draw the player
-    var half = blockSizex;
-    var img = document.getElementById("scream");
-    ctx.drawImage(img,player.x*blockSizex+half, player.y*blockSizey+half, -blockSizex, -blockSizey);
+    var img = document.getElementById("player");
+    ctx.drawImage(img,player.x*blockSizex, player.y*blockSizey, blockSizex, blockSizey);
 
 }
 
 //Check to see if the new space is inside the board and not a wall
 function canMove(x, y){
-    return (y>=0) && (y<boardOne.length) && (x >= 0) && (x < boardOne[y].length) && (boardOne[y][x] != 1);
+    return (y>=0) && (y<board.length) && (x >= 0) && (x < board[y].length) && (board[y][x] != 1);
 }
 
+
+//objects, coins, obstacles, finish
 function win(x, y)
 {
-    if (boardOne[y][x] == -1)
+    if (board[y][x] == -1)
     {
-        console.log("hi")
+        console.log("WOO! i win!!")
     }
-    
-
 }
+function obstacleS(x, y)
+{
+    if (board[y][x] == 4 || board[y][x] == 3 ||board[y][x] == 5)
+    {
+        console.log("ouch")
+    }  
+}
+
 
 $(document).keydown(function(e){
     win(player.x, player.y);
-    if((e.which == 38) && canMove(player.x, player.y-1) )//Up arrow
+    obstacleS(player.x, player.y);
+    if((e.which == 38) && canMove(player.x, player.y-1))
+    {
         player.y--;
+    }
         
-    else if((e.which == 40) && canMove(player.x, player.y+1)) // down arrow
+    else if((e.which == 40) && canMove(player.x, player.y+1))
+    {
         player.y++;
+    }
     else if((e.which == 37) && canMove(player.x-1, player.y))
+    {
         player.x--;
+    }
     else if((e.which == 39) && canMove(player.x+1, player.y))
+    {
         player.x++;
+    }
     draw();
     e.preventDefault();
 });
+$(document).keyup(function(e){
+    obstacleS(player.x, player.y);
+    win(player.x, player.y);
+});
 
 draw();
+
+
+
+//level switch
+
+function lOne ()
+{
+    document.getElementById("GameBoardCanvas").width = ("1100")
+    document.getElementById("GameBoardCanvas").height = ("1900")
+    draw(board = boardOne);
+}
+function lTwo ()
+{    
+    document.getElementById("GameBoardCanvas").width = ("1100")
+    document.getElementById("GameBoardCanvas").height = ("1000")
+    draw(board = boardTwo);
+}
+function lThree ()
+{ 
+    document.getElementById("GameBoardCanvas").width = ("1100")
+    document.getElementById("GameBoardCanvas").height = ("1000")
+    draw(board = boardThree);
+}
