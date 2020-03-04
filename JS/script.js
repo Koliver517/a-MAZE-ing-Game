cfea806ea52c47973880b1c63e8a9fb12ae40d1a
Matthew Crowell <overthewelldesigns@gmail.com>
2020-03-04 15:25:25 -0700

    added movine enemys

--- JS/script.js
+++ JS/script.js
@@ -28,8 +28,8 @@ var loadThree = false;
 //The game board 1 = walls, 0 = free space, and -1 = the goal, 2 = chest
 
     var boardOne = [
-        [ 0, 5, 4, 3, 2, 0, 0, 0, 0, 0, 1],
-        [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [ 0, 0, 4, 3, 2, 0, 0, 0, 0, 0, 1],
+        [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1],
         [ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
         [ 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1],
         [ 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1],
@@ -79,10 +79,18 @@ var loadThree = false;
         x: 0,
         y: 0
     };
-
+    var enemlione=
+    {
+        x: 1, y:1
+    };
+    var enemliTwo=
+    {
+        x: 9, y:1
+    };
     board = boardOne
 //Draw the game board
 function draw(){
+
     var canvas = $('#GameBoardCanvas');
     var width = canvas.width();
     var height = canvas.height();
@@ -90,7 +98,7 @@ function draw(){
     var blockSizey = height/board.length;
     var ctx = canvas[0].getContext('2d');
     ctx.clearRect(0, 0, width, height);
-    ctx.fillStyle="white";
+    ctx.fillStyle="#027200";
     //Loop through the board array drawing the walls and the goal
     for(var y = 0; y < board.length; y++){
         for(var x = 0; x < board[y].length; x++){
@@ -122,7 +130,7 @@ function draw(){
             //Draw the second enemy
             else if(board[y][x] === 4)
             {
-                var img = document.getElementById("enemyTwo");
+                var img = document.getElementById("enemyOne");
                 ctx.drawImage(img,x*blockSizex, y*blockSizey, blockSizex, blockSizey);
             }
              //Draw the third enemy
@@ -131,22 +139,33 @@ function draw(){
                 var img = document.getElementById("enemyThree");
                 ctx.drawImage(img,x*blockSizex, y*blockSizey, blockSizex, blockSizey);
             }
-            
 
             
         }
     }
-    //Draw the player
+    //Draw the player and moving enemys
+    var img = document.getElementById("enemyFive");
+    ctx.drawImage(img,enemlione.x*blockSizex, enemlione.y*blockSizey, blockSizex, blockSizey);
+    
+    var img = document.getElementById("enemyTwo");
+    ctx.drawImage(img,enemliTwo.x*blockSizex, enemliTwo.y*blockSizey, blockSizex, blockSizey);
+
     var img = document.getElementById("player");
     ctx.drawImage(img,player.x*blockSizex, player.y*blockSizey, blockSizex, blockSizey);
 
-}
+
+    //draw moving enemy
+
+
+        
+    }
+
+
 
 //Check to see if the new space is inside the board and not a wall
 function canMove(x, y){
     return (y>=0) && (y<board.length) && (x >= 0) && (x < board[y].length) && (board[y][x] != 1);
 }
-
 
 //objects, coins, obstacles, finish
 function win(x, y)
@@ -158,9 +177,13 @@ function win(x, y)
 }
 function obstacleS(x, y)
 {
-    if (board[y][x] == 4 || board[y][x] == 3 ||board[y][x] == 5)
+    if (board[y][x] == 4 || board[y][x] == 3 ||board[y][x] == 5 || (player.x == enemlione.x && player.y == enemlione.y)  || (player.x == enemliTwo.x && player.y == enemliTwo.y))
     {
         console.log("ouch")
+        document.getElementById("GameBoardCanvas").width = ("1100")
+        document.getElementById("GameBoardCanvas").height = ("1900")
+        draw(board = boardOne, player = {x: 0,y: 0});
+
     }  
 }
 
@@ -185,9 +208,29 @@ $(document).keydown(function(e){
     {
         player.x++;
     }
+    //enemy
+    if((e.which == 37) && canMove(enemlione.x, enemlione.y-1))
+    {
+        enemlione.y--;
+    }
+    else if((e.which == 39) && canMove(enemlione.x, enemlione.y+1))
+    {
+        enemlione.y++;
+    }
+    //enemy two
+    if((e.which == 38) && canMove(enemliTwo.x-1, enemliTwo.y))
+    {
+        enemliTwo.x--;
+    }
+    else if((e.which == 40) && canMove(enemliTwo.x+1, enemliTwo.y))
+    {
+        enemliTwo.x++;
+    }
+
     draw();
     e.preventDefault();
 });
+
 $(document).keyup(function(e){
     obstacleS(player.x, player.y);
     win(player.x, player.y);
@@ -203,17 +246,17 @@ function lOne ()
 {
     document.getElementById("GameBoardCanvas").width = ("1100")
     document.getElementById("GameBoardCanvas").height = ("1900")
-    draw(board = boardOne);
+    draw(board = boardOne, player = {x: 0,y: 0});
 }
 function lTwo ()
 {    
     document.getElementById("GameBoardCanvas").width = ("1100")
     document.getElementById("GameBoardCanvas").height = ("1000")
-    draw(board = boardTwo);
+    draw(board = boardTwo, player = {x: 0,y: 0});
 }
 function lThree ()
 { 
     document.getElementById("GameBoardCanvas").width = ("1100")
     document.getElementById("GameBoardCanvas").height = ("1000")
-    draw(board = boardThree);
+    draw(board = boardThree, player = {x: 0,y: 0});
 }
